e4b0eddfa4321ae5810b5523a6184565c8543bb3
alistaircolling <alistair.colling@gmail.com>
2015-09-14 13:19:51 +0100

    init

---
+++ src/app/index.run.ts
@@ -0,0 +1,11 @@
+module generatorGulpAngular {
+  'use strict';
+
+  export class RunBlock {
+    /** @ngInject */
+    constructor($log: ng.ILogService) {
+      $log.debug('runBlock end');
+    }
+
+  }
+}
